7acdbbe4b9859285be55f5a683b52169daa51fa9
Giuseppe Capizzi <gcapizzi@pivotal.io>
2018-02-27 18:11:00 +0000

    Don't import Data.Hashmap.Strict qualified

--- src/JMESPath/Core.hs
+++ src/JMESPath/Core.hs
@@ -4,13 +4,13 @@ module JMESPath.Core
     ) where
 
 import Data.Aeson
-import qualified Data.HashMap.Strict as HashMap
+import Data.HashMap.Strict
 import Data.Text (Text)
 
 data Expression = Identifier Text
                 | SubExpression Expression Expression
 
 searchValue :: Expression -> Value -> Either String Value
-searchValue (Identifier identifier) (Object object) = Right $ HashMap.lookupDefault Null identifier object
+searchValue (Identifier identifier) (Object object) = Right $ lookupDefault Null identifier object
 searchValue (SubExpression left right) document = searchValue left document >>= searchValue right
 searchValue _ _ = Right Null
